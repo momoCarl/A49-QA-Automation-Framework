94f4d3386499cb24aad8afdb87496c83a6ba4257
momoCarl <momo.carlos5@gmail.com>
2023-08-25 16:27:14 -0400

    Update Homework16.java

---
+++ src/test/java/Homework17.java
@@ -0,0 +1,67 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Homework17 extends BaseTest{
+
+
+@Test
+public void addSongToPlaylist(){
+openLoginUrl("https://qa.koel.app/");
+provideEmail("momo.carlos5@gmail.com");
+providePassword("te$t$tudent");
+cliquesubmit();
+SearchSongs("BossStatus");
+clickViewAll();
+addSongToMyPlaylist();
+
+
+    
+        }
+
+    private void addSongToMyPlaylist() {
+        driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/div/div/div[1]/table/tr")).click();
+        driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/header/div[3]/span/button[2]")).click();
+       driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/header/div[3]/div/section[1]/ul/li[5]")).click();
+        WebElement addSong = driver.findElement(By.xpath("/html/body/div[2]/div"));
+        Assert.assertTrue(addSong.isDisplayed());
+
+
+    }
+
+    private void clickViewAll() {
+    driver.findElement(By.xpath("//*[@id='searchExcerptsWrapper']/div/div/section[1]/h1/button")).click();
+    }
+
+    private void SearchSongs(String s) {
+        WebElement search = driver.findElement(By.xpath("//*[@id=\"searchForm\"]/input"));
+        search.clear();
+        search.sendKeys(s);
+    }
+
+    private void cliquesubmit() {
+        driver.findElement(By.cssSelector("[type=submit]")).click();
+    }
+
+    private void openLoginUrl(String url) {
+      driver.get(url);
+    }
+
+
+    public void providePassword(String p ) {
+        driver.findElement(By.cssSelector("input[placeholder=\"Password\"]")).sendKeys(p);
+
+    }
+
+    public void provideEmail(String e) {
+        WebElement email = driver.findElement(By.cssSelector("input[placeholder=\"Email Address\"]"));
+        email.clear();
+        email.sendKeys(e );
+    }
+
+
+}
