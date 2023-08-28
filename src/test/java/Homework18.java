b8f5423060a2bc89b5b4916c84048a93d8c34489
momoCarl <momo.carlos5@gmail.com>
2023-08-27 20:20:21 -0400

    Update Homework17.java

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework18 extends BaseTest {
+    @Test
+    public void playSong() {
+        openLoginUrl("https://qa.koel.app/");
+        loginValideEmailPassowrd("momo.carlos5@gmail.com", "te$t$tudent");
+        playNextSong();
+        clickPlaySong();
+    }
+
+    private void clickPlaySong() {
+        driver.findElement(By.cssSelector("[title='Play or resume']")).click();
+        WebElement pause = driver.findElement(By.cssSelector("[title='Pause']"));
+        Assert.assertTrue(pause.isDisplayed());
+    }
+
+    private void playNextSong() {
+        driver.findElement(By.xpath("//*[@id='mainFooter']/div[1]/i[2]")).click();
+    }
+
+    private void loginValideEmailPassowrd(String e, String p) {
+        WebElement email = driver.findElement(By.cssSelector("input[placeholder=\"Email Address\"]"));
+        email.clear();
+        email.sendKeys(e);
+        driver.findElement(By.cssSelector("[placeholder=\"Password\"] ")).sendKeys(p);
+        driver.findElement(By.cssSelector("[type=submit]")).click();
+    }
+
+    private void openLoginUrl(String url) {
+        driver.get(url);
+    }
+}
