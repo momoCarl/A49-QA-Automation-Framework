2b1144345496c1257edbe10a2a26592f8e7a35fa
momoCarl <momo.carlos5@gmail.com>
2023-08-28 23:05:08 -0400

    Update Homework17.java

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,16 +1,31 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Homework19 extends BaseTest {
-    String url;
 
-    @Test
     @Parameters({"BaseURL"})
+    @Test
     public void deletePlaylist(String baseUrl) {
         driver.get(baseUrl);
         loginValideEmailPassowrd("momo.carlos5@gmail.com", "te$t$tudent");
+        clickPlayList();
+        clickDeletePlaylist();
+    }
+
+    private void clickDeletePlaylist() {
+        driver.findElement(By.cssSelector("[title='Delete this playlist']")).click();
+        WebElement notificationDeleted = driver.findElement(By.xpath("/html/body/div[2]/div"));
+        Assert.assertTrue(notificationDeleted.isDisplayed());
+    }
+
+    private void clickPlayList() {
+        driver.findElement(By.xpath("//*[@id=\"playlists\"]/ul/li[4]/a")).click();
+        WebElement xPlaylist = driver.findElement(By.cssSelector("[title='Delete this playlist']"));
+        Assert.assertTrue(xPlaylist.isDisplayed());
     }
 
     private void loginValideEmailPassowrd(String e, String p) {
