4efed3d1eb610cbd4fa2002602d4acea79006069
momoCarl <momo.carlos5@gmail.com>
2023-09-09 16:19:06 -0400

    Update Homework17.java

--- src/test/java/pages/AlbumPage.java
+++ src/test/java/pages/AlbumPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openxmlformats.schemas.drawingml.x2006.chart.STHPercentUShort;
 import org.testng.Assert;
 
 public class AlbumPage extends BasePage{
@@ -26,22 +27,26 @@ public class AlbumPage extends BasePage{
     public AlbumPage(WebDriver driver) {
         super(driver);
     }
-    public void clickAlbums(){
+    public AlbumPage clickAlbums(){
         wait.until(ExpectedConditions.elementToBeClickable(albums)).click();
+        return this;
     }
 
-    public void clickPlayAll(){
+    public AlbumPage clickPlayAll(){
         wait.until(ExpectedConditions.elementToBeClickable(playbAll)).click();
         wait.until(ExpectedConditions.visibilityOf(sound));
         Assert.assertTrue(sound.isDisplayed());
+        return this;
     }
-    public void shufflePlay(){
+    public AlbumPage shufflePlay(){
         wait.until(ExpectedConditions.elementToBeClickable(shuffle)).click();
         wait.until(ExpectedConditions.visibilityOf(sound));
         Assert.assertTrue(sound.isDisplayed());
+        return this;
     }
-    public void clickRigthBtnPlay(){
+    public AlbumPage clickRigthBtnPlay(){
         action.contextClick(wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Play all songs in the album Airbit")))).perform();
+        return this;
     }
     public AlbumPage checkIfTitleIsVisible(){
         wait.until(ExpectedConditions.visibilityOf(title));
